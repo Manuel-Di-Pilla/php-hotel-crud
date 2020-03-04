40c0085df09cefb85c9d49b2269855c561ac0167
Manuel <manueldipilla3@gmail.com>
2020-03-04 17:06:53 +0100

    prima parte

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+  include 'database.php';
+?>
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>hotel</title>
+    <link rel="stylesheet" href="dist/app.css">
+  </head>
+  <body>
+
+  </body>
+</html>
